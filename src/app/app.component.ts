a8d85d93c18fc0602dc799f90fd66486e6d86a8d
gonmarfer2 <martinezfernandezgonzalo@gmail.com>
2023-08-23 19:56:44 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.2.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'micv';
+}
